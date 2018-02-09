355ba974973f2aaf5d805c29cf9ff47cf1effacb
PECH Guilhem <p16001500@a-maq-iutinfo.salsa.univ-amu.fr>
2018-02-09 12:04:29 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+(function() {
+    "use strict";
+    let css_blanc = {
+        'background-color': 'black',
+        'color': 'white',
+        'font-weight': 'bold',
+        'border': 'solid 1px white'
+    }
+    let css_noire = {
+        'background-color': 'white',
+        'color': 'black',
+        'border': 'solid 1px black',
+        'font-weight': 'bold'
+    }
+
+    let css_rouge = {
+        'background-color': 'red',
+        'color': 'yellow',
+        'border': 'solid 1px black',
+        'font-weight': 'bold'
+    }
+    $(document).ready(function() {
+        /* On peut travailler sur le DOM ! */
+        let damier = $('#damier')
+
+        for (let i = 0; i < 8; i++) {
+          let tr = $('<tr />');
+          for (var j = 0; j < 10; j++) {
+            tr.append($('<td />').addClass(((j+i)%2 == 0 ? 'case-blanche':'case-noire')).html('TD'))
+          }
+          damier.append(tr);
+        }
+
+
+        let case_blanche = $('.case-blanche');
+        let case_noire = $('.case-noire');
+
+
+        case_blanche.css(css_blanc).hover(function(event) {
+            $(this).css(css_rouge);
+        }, function() {
+            $(this).css(css_blanc);
+        });
+
+        case_noire.css(css_noire).hover(function(event) {
+            $(this).css(css_rouge);
+        }, function() {
+            $(this).css(css_noire);
+        });
+
+
+    });
+})();
